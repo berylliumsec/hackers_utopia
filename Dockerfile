b7e032dec3a267d0da92520e55a0563400a82f4f
petusawo <david.ige@gmail.com>
2022-12-07 08:44:17 -0500

    adding aws envs

--- Dockerfile
+++ Dockerfile
@@ -18,7 +18,8 @@ RUN apt update -y && apt upgrade -y && apt-get autoremove -y && apt-get clean -y
     curl \
     expect \
     wireshark \
-    unzip
+    unzip \
+    less
 
 RUN pip3 install \
     boto3 
@@ -41,7 +42,7 @@ RUN wget https://download.java.net/java/GA/jdk19/877d6127e982470ba2a7faa31cc93d0
 RUN tar zxvf openjdk-19_linux-x64_bin.tar.gz && mv jdk-19 /usr/local
 RUN echo 'JAVA_HOME="/usr/local/jdk-19"' >> ~/.bashrc
 RUN echo 'PATH=$PATH:$JAVA_HOME/bin' >> ~/.bashrc
-COPY discover_aws_services.py ./
+COPY discover_aws_services.py set_aws_envs.py ./
 RUN chmod +x burpsuite_pro_linux_v2022_9_6.sh burpsuite_community_linux_v2022_9_6.sh burpsuite_pro burpsuite_community
 RUN ./burpsuite_pro && ./burpsuite_community
 RUN cd /usr/share/nmap/scripts/ && \
---
+++ set_aws_envs.py
@@ -0,0 +1,35 @@
+import boto3
+import logging
+import os
+import argparse
+
+logging.basicConfig(level=logging.DEBUG)
+
+parser = argparse.ArgumentParser(description="Configure")
+
+parser.add_argument(
+    "--Region",
+    type=str,
+    metavar="Region",
+    help="Region",
+)
+parser.add_argument(
+    "--Serial_number",
+    type=str,
+    metavar="Serial_number",
+    help="Serial_number",
+)
+def get_services():
+    client =  boto3.client("sts", region_name=args.Region)
+    response = client.get_session_token(
+    SerialNumber=args.Serial_number,
+    TokenCode=args.Token
+)
+    logging.debug(response)
+    os.putenv("AccessKeyId" , response["Credentials"]["AccessKeyId"])
+    os.putenv("SecretAccessKey" , response["Credentials"]["SecretAccessKey"])
+    os.putenv("TokenCode" , response["Credentials"]["TokenCode"])
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
